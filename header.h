77a826c24fe628a33d0f73f9499ff38087dfddf8
George Papanikolaou <g3orge.app@gmail.com>
2011-12-01 14:57:31 +0200

    First header file

---
+++ header.h
@@ -0,0 +1,8 @@
+// This is the main header file
+
+#include <iostream>
+#include <cstring>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 // This is the main file for the proect where the magic happens.
-
-#include <iostream>
-using namespace std;
+#include "header.h"
 
 int main() {
-
+    cout << "Thing of course";
     return 0;
 }
