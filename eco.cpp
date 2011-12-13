e0e5544301e1792bd4c7ea0c2e59228f485cb780
George Papanikolaou <g3orge.app@gmail.com>
2011-12-13 14:39:38 +0200

    One file

---
+++ eco.cpp
@@ -0,0 +1,28 @@
+// This is the first file of the C++ Project 'EcoSystem' , that I'm working on.
+// I'm gonna do it all in one file and split it later maybe
+
+#include <iostream>
+#include <cstring>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+srand (time(NULL));
+
+class Organism
+{
+    private:
+        int age;
+        int size;
+        int gp;     //Growth Percentage
+        bool reproduction;
+        bool sex;
+    public:
+        
+}
+
+int main()
+{
+    cout << "Thing of course" << endl;
+    return 0;
+}
