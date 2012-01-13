618af021db8126ecc0767d33538a14552c25105c
George Papanikolaou <g3orge.app@gmail.com>
2012-01-13 02:19:58 +0200

    This is a whole new implementation. Basically, v2

--- eco.cpp
+++ eco.cpp
@@ -9,22 +9,22 @@
 
 using namespace std;
 
-// This is a system function to generate random numbers where I need.
+/* This is a system function to generate random numbers where I need. */
 int RandomInteger(int low, int high) {  
-      int k;  
-      int dif=high-low+1; 
-      k = low + rand() % dif; 
-      return k;  
+    int k;
+    int dif=high-low+1; 
+    k = low + rand() % dif; 
+    return k;  
 } 
 
 // This is the main function for time operations
-void wait (int seconds) {
+void wait(int seconds) {
     clock_t endwait;
     endwait = clock () + seconds * CLOCKS_PER_SEC;
     while (clock() < endwait) {/* Nothing to see here */}
 }
 
-// Declaration of the classes begins here =========================================
+/* ========================= Declaration of the classes begins here =========================== */
 class Organism {
     private:
         int age;
@@ -34,7 +34,7 @@ class Organism {
         bool sex;       // 0 for female, 1 for male
         void interaction() {
             // This is the main interaction fucntion for stuff like eating and reproduction
-            if ( /* The master class is the same -> reproduction, else eating if possible*/ ) {}
+            if ( /* The organism is the same -> reproduction, else eating if possible*/ ) {}
         }
     public:
         void move() {
@@ -60,63 +60,57 @@ class Phytoplankton: public Plankton {
     private:
     public:
 };
-// Declaration of the classes ends here ==========================================
+/* ========================= Declaration of the classes ends here =========================== */
 
 int main() {
+    restart_label:
     srand((int)time(0));
-    int choice;
-    bool running = false;
-    cout << "Welcome - Please select one of the functions:" << endl;
-    cout << "1 - Start\n2 - Pause\n3 - Resume\n4 - Restart\n5- Add new object\n6- Get info\nElse to quit" << endl;
+    cout << "Generating the system" << endl;
+    wait(1);
+    int x=RandomInteger(15,20);
+    int y=RandomInteger(15,20);
+    Organism map[x][y];
+    cout << "The map has " << x << " length and" << y << " width" << endl;
+    int N = x*y;
+    cout << "Placing organisms into the map" << endl;
+    for (int i=0; i=(N/2)+2; i++) {
+        int place_x = RandomInteger(1,N);
+        int place_y = RandomInteger(1,N);
+        map[place_x][place_y] = Phytoplankton o1;
+        /* Create more organisms here (problably with another function or class) -------- */
+    }
+    char choice;
+    cout << "Press C to make a step\nP to print the map\nR to restart\nI to get info\nAnything else to quit" << endl;
     cin >> choice;
-    while (choice == 1 || choice == 2 || choice == 3 || choice == 4 || choice == 5 || choice == 6) {
-        if (choice == 1) {
-            if (running=true)
-                cout << "System already running" << endl;
-            else {
-                // <--- Generating system here --->
-                clock_t run_clock = clock();
-                int x=RandomInteger(9,13);
-                int y=RandomInteger(9,13);
-                Organism map[x][y];
-                cout << "Generating the map" << endl;
-                cout << "The map has " << x << " length and" << y << " width" << endl;
-                int N = x*y;
-                cout << "Placing organisms into the map" << endl;
-                for (int i=0; i=N/2; i++) {
-                    int place_x = RandomInteger(1,N);
-                    int place_y = RandomInteger(1,N);
-                    map [place_x][place_y] = Phytoplankton o1;
-                    // Create more organisms here (problably with another function or class)
+    double time_now; // Or clock_t type
+    int step_counter = 1;
+    while (choice == 'C' || choice == 'c') {
+        time_now = clock();
+        /* <-------Actions for one step --------> */
+        cout << "Press C to make a step\nP to print the map\nR to restart\nI to get info\nAnything else to quit" << endl;
+        getline (cin,choice);
+        step_counter++;
+    }
+    if (choice == 'R' || choice == 'r')
+        goto restart_label;
+    else if (choice == 'I' || choice == 'i') {
+        cout << "Press 1 to get general info or the character of the organism for specific" << endl;
+        // Return to the main loop
+    }
+    else if (choice == 'A' || choice == 'a') {
+        /* <---Add "Organism/Comdition manipulation code" here ---> */
+        // Return to the main loop
+    }
+    else if (choice == 'P' || choice == 'p') {
+        int i,j;
+        for (i=0; i=x; i++) {
+            for (j=0; j=y; j++) {
+                if ( map[i][j] == /*SOME ORGANISM*/ ) {
+                    cout << "letter for the orgnism";
                 }
             }
+            cout << endl; //Change the line in the stdout
         }
-        else if (choice == 2) {
-            if (running=false)
-                cout << "System not running" << endl;
-            else {
-                run_clock = 0;
-                cout << "System paused" << endl;
-            }
-        }
-        else if (choice == 3) {
-            if (running=true)
-                cout << "System already running" << endl;
-            else {
-                //Resume system here:
-            }
-        }
-        else if (choice == 4) {
-            // Restart system here
-        }
-        else if (choice == 5) {
-            //Add new object to the list and the system (with user input) here
-        }    
-        else {
-            // Code to give info to the user here (user input) statistics, organism info etc. 
-        }
-        cout << "1 - Start\n2 - Pause\n3 - Resume\n4 - Restart\n5- Add new object\n6- Get info\nElse to quit" << endl;
-        cin >> choice;
     }
-    return 0;
+    else { cout << "System quitting after " << clock()/CLOCKS_PER_SEC << " seconds" << endl; }
 }
